459f1a5e93e9607af15afa3eac5c086867aedfa2
최유정 <choeyujeong@choeyujeongs-MacBook-Pro.local>
2026-01-07 22:49:50 +0900

    [fix] 메인, 로그인 디자인 수정(tailwind로 변경)

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,20 +1,31 @@
 import { Link, useLocation } from 'react-router-dom'
 
 export default function TopNav() {
-    const { pathname } = useLocation();
+  const { pathname } = useLocation()
 
-    const isLogin = pathname === '/login';
-    const isSignup = pathname === '/signup';
-    
-    return (
-        <div className="top-nav">
-            <Link className={`top-link ${isLogin ? 'active' : ''}`} to="/login">
-            Login
-            </Link>
-            <span className="top-line">|</span>
-            <Link className={`top-link ${isSignup ? 'active' : ''}`} to="/signup">
-            Sign up
-            </Link>
-        </div>
-    )
+  const isLogin = pathname === '/login'
+  const isSignup = pathname === '/signup'
+
+  const baseClass = 'text-[#1e1e1e] fontRegular text-[14px] px-[0.5%] py-[0.3%] tracking-wide no-underline hover:no-underline'
+  const activeClass = 'font-bold text-black'
+
+  return (
+    <div className="flex justify-end items-center">
+      <Link
+        to="/login"
+        className={`${baseClass} ${isLogin ? activeClass : ''}`}
+      >
+        Login
+      </Link>
+
+      <span className="mx-[5px] text-[#1e1e1e]">|</span>
+
+      <Link
+        to="/signup"
+        className={`${baseClass} ${isSignup ? activeClass : ''}`}
+      >
+        Sign up
+      </Link>
+    </div>
+  )
 }
