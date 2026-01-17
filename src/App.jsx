320db43b35b007372142f34c708f1b3ea9c38ffb
최유정 <choeyujeong@choeyujeongs-MacBook-Pro.local>
2026-01-17 22:26:42 +0900

    add: 비밀번호 찾기 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main.jsx';
 import Login from './pages/Login.jsx';
 import SignUp from './pages/SignUp.jsx';
+import FindPassword from './pages/FindPassword.jsx';
 import ProjectList from './pages/Project/ProjectList.jsx';
 import ProjectListDetail from './pages/Project/ProjectListDetail.jsx';
 import PlanningDetail from './pages/Project/PlanningDetail.jsx';
@@ -12,6 +13,7 @@ export default function App() {
       <Route path="/" element={<Main />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/findPassword" element={<FindPassword />} />
       <Route path="/projectList" element={<ProjectList />} />
       <Route path="/projectList/:projectId" element={<ProjectListDetail />} />
       <Route path="/projectList/:projectId/planning" element={<PlanningDetail />} />
